23a6c63cdc5b7a32b1228f6d94855d915061bd7b
jaskola <bartosz.michalowski.j@gmail.com>
2018-11-22 15:28:20 +0100

    Delegated more settings to json file

--- server/django_rest_api/django_rest_api/settings.py
+++ server/django_rest_api/django_rest_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = CONFIG["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = CONFIG["ALLOWED_HOSTS"]
 
 
 # Application definition

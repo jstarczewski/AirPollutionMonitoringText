a2d3262bef3d851554e8803e547df4a9d629b8b7
jaskola <bartosz.michalowski.j@gmail.com>
2018-11-22 12:16:09 +0100

    Created api app with Django models for Sensor, Measurement and Pollutant. Minor settings fixes for database compatibility. Added .gitignore for server files.

--- server/django_rest_api/api/models.py
+++ server/django_rest_api/api/models.py
@@ -18,5 +18,5 @@ class Measurement(models.Model):
     pollution_level = models.CharField('Air pollution level', max_length=10)
     timestamp = models.DateTimeField('Timestamp', auto_now=True)
     sensor = models.ForeignKey('Sensor', on_delete=models.CASCADE)
-    pollutants_values = JSONField("Pollutants' Value")
+    pollutants_values = JSONField("Pollutants' Value", null=True)
 
